6d71170ab9c8460b30f96613081e37e8a4691edf
rubygeek <nola@rubygeek.com>
2018-10-08 19:54:56 -0500

    added tests for language

---
+++ test/language/core_test.clj
@@ -0,0 +1,17 @@
+(ns language.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [language.core :as lang]))
+
+(deftest make-words
+  (let [one (lang/make-english "one")
+        uno (lang/make-spanish "uno")]
+    (testing "testing helpers"
+      (is (= {:Language :English :word "one"} one)))
+      (is (= {:Language :Spanish :word "uno"} uno))))
+
+(deftest translate-converts-words
+  (let [one (lang/make-english "one")
+        uno (lang/make-spanish "uno")]
+    (testing "translates correctly"
+      (is (= (lang/translate one) "uno")))
+      (is (= (lang/translate uno) "one"))))
